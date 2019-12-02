a9ac81487976e027df6f0e72398cc9d029b96976
Luana Porciuncula Barreto <lpb@ug3c25.windows.cin.ufpe.br>
2019-12-02 10:02:05 -0300

    implementação da nav-bar com barra de pesquisa

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -2,28 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router'
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 import { ListaDesejosComponent } from './lista-desejos/lista-desejos.component';
 import { EstoqueService } from './EstoqueService';
 import { ProdutosService } from './ProdutosService';
 import { PaginaProdutoComponent } from './PaginaProduto.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaDesejosComponent,
-    PaginaProdutoComponent
+    PaginaProdutoComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    MatToolbarModule,
     RouterModule.forRoot([
       {
-        path: 'ListaDesejos',
+        path: 'lista-desejos',
         component: ListaDesejosComponent
       },
       {
@@ -31,6 +36,9 @@ import { PaginaProdutoComponent } from './PaginaProduto.component';
         component: PaginaProdutoComponent
       }
     ])
+  ],
+  exports: [
+    MatToolbarModule,
   ],
   providers: [EstoqueService, ProdutosService],
   bootstrap: [AppComponent]
