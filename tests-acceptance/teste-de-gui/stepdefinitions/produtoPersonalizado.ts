5a1e2de599315fad10e6ebaeb151441de2eddf3f
viniciussousav <vsv@cin.ufpe.br>
2019-12-10 15:55:11 -0300

    [#26]Pedido de produto personalizado com informações incompletas

--- tests-acceptance/teste-de-gui/stepdefinitions/produtoPersonalizado.ts
+++ tests-acceptance/teste-de-gui/stepdefinitions/produtoPersonalizado.ts
@@ -29,3 +29,28 @@ defineSupportCode(function( {Given, When, Then}) {
         expect(Promise.resolve(pedidos.length)).to.eventually.equal(1);
     });
 })
+
+defineSupportCode(function( {Given, When, Then}) {
+    Given(/^I am logged as "([^\"]*)"$/, async (usuario) => {
+        var usr = element(by.name('user'));
+        await expect(usr.name()).to.eventually.equal(usuario);
+    });
+    Given(/^I am on "Produto Personalizado" page$/, async() => {
+        await browser.get("http://localhost:3000/personalizarProduto");
+    });
+    Given(/^I choose "([^\"]*)","([^\"]*)", "(\d*)" and "([^\"]*)"$/, async(categoria, cor, quantidade, mensagem) => {
+        await $("input[name='categoria']").sendKeys(<string>categoria);
+        await $("input[name='quantidade']").sendKeys(<string>quantidade);
+        await $("input[name='mensagem']").sendKeys(<string>mensagem);   
+    });
+    When(/^I ask the system to "Finalizar"$/, async () => {
+        await element(by.buttonText('Finalizar')).click();
+    });
+    Then(/^Then I am at “Produto Personalizado” page $/, async(nome, pagina) => {
+        await browser.get("http://localhost:3000/personalizarProduto");
+    });
+    Then(/^see "Preencha todos os campos" message$/, async () => {
+        //var mensagem = element(by.name('aviso'));
+        //await expect(mensagem).to.eventually.equal("Preencha todos os campos"););
+    });
+})
